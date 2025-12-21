03fbd945625bf07b178716ed1ae61d3a5388dbf5
kubrabas <kubraaabasss@gmail.com>
2025-12-21 14:09:19 +0100

    clean deprecated methods

---
+++ MyClasses/truth_extractor.py
@@ -0,0 +1,15 @@
+from graphnet.data.extractors.icecube.utilities.frames import frame_is_montecarlo, frame_is_noise
+
+
+
+class I3TruthExtractorPONE(I3TruthExtractor):
+
+
+
+
+
+# Test__I3TruthExtractor_PONE_by_LeptonInjector.ipynb adli dosyayi da bunun oldugu folder'a koy!!
+# duzenle folder'lariiiiiiii.
+
+
+
