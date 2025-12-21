520a6512a3b06a9f3d35a487f315370f8bbc4ee2
kubrabas <kubraaabasss@gmail.com>
2025-12-21 14:09:35 +0100

    clean deprecated methods

--- MyClasses/truth_extractor.py
+++ MyClasses/truth_extractor.py
@@ -11,5 +11,3 @@ class I3TruthExtractorPONE(I3TruthExtractor):
 # Test__I3TruthExtractor_PONE_by_LeptonInjector.ipynb adli dosyayi da bunun oldugu folder'a koy!!
 # duzenle folder'lariiiiiiii.
 
-
-
