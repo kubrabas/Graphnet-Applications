ba80d533da6fac5d2172ab3fd482f442b8534da3
kubrabas <kubraaabasss@gmail.com>
2025-12-22 21:19:43 +0100

    edit __init__ to include truth extractor

--- MyClasses/__init__.py
+++ MyClasses/__init__.py
@@ -1,5 +1,6 @@
 from .feature_extractor import I3FeatureExtractorPONE
+from .truth_extractor import I3TruthExtractorPONE
 from .reader import PONE_Reader
 
 
-__all__ = ["I3FeatureExtractorPONE", "PONE_Reader"]
+__all__ = ["I3FeatureExtractorPONE", "PONE_Reader", "I3TruthExtractorPONE"]
