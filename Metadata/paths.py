2c93789ed2665ce712512b9b55b359965d7061b5
kubrabas <kubraaabasss@gmail.com>
2026-04-18 20:52:25 +0200

    add paths

---
+++ Metadata/paths.py
@@ -0,0 +1,175 @@
+import os
+
+# ============================================================
+# GCD Files
+# ============================================================
+
+GCD = {
+    "Spring2026MC": "/project/6008051/pone_simulation/GCD_Library/PONE_800mGrid_40mSpacing_40OMstring.i3.gz",
+    "340StringMC":  "/project/6008051/pone_simulation/GCD_Library/PONE_800mGrid.i3.gz",
+}
+
+
+# ============================================================
+# Spring 2026 MC - I3 Folders
+# ============================================================
+
+SPRING2026MC_I3 = {
+    "full_geometry": {
+        "Muon":     {"path": "/project/6008051/pone_simulation/MC000008-nu_mu-2_6-LeptonInjector_PROPOSAL_clsim-v17.1/Generator",   "format": "zst"},
+        "Electron": {"path": "/project/6008051/pone_simulation/MC000009-nu_e-2_6-LeptonInjector_PROPOSAL_clsim-v17.1/Generator",   "format": "zst"},
+        "Tau":      {"path": "/project/6008051/pone_simulation/MC000010-nu_tau-2_6-LeptonInjector_PROPOSAL_clsim-v17.1/Generator",  "format": "zst"},
+        "NC":       {"path": "/project/6008051/pone_simulation/MC000011-nu_NC-2_6-LeptonInjector_PROPOSAL_clsim_NC-v17.1/Generator","format": "zst"},
+    },
+    "strings_102_40m": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "strings_102_80m": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+}
+
+
+# ============================================================
+# Spring 2026 MC - PMT Folders
+# ============================================================
+
+SPRING2026MC_PMT = {
+    "full_geometry": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "strings_102_40m": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "strings_102_80m": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+}
+
+
+# ============================================================
+# 340 String MC - I3 Folders
+# ============================================================
+
+STRING340MC_I3 = {
+    "full_geometry": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "102_string": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "160_string": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "compact": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "default": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "expanded": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "large": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "modified": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+}
+
+
+# ============================================================
+# 340 String MC - PMT Folders
+# ============================================================
+
+STRING340MC_PMT = {
+    "full_geometry": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "102_string": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "160_string": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "compact": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "default": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "expanded": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "large": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+    "modified": {
+        "Muon":     {"path": None, "format": None},
+        "Electron": {"path": None, "format": None},
+        "Tau":      {"path": None, "format": None},
+        "NC":       {"path": None, "format": None},
+    },
+}
+
